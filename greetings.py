ab9aace5f3d79f44d3e87b20d0f9fb0277f27cf4
Belashovva <belashova.com@mail.ru>
2024-12-22 20:45:23 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,13 @@
+username = "Подопытный заметочник 1"
+title = "Заметка проекта"
+content = "Тестим"
+status = "OPEN"
+created_date = "22-12-2024"
+issue_date = "17-06-2025"
+
+print(username)
+print(title)
+print(content)
+print(status)
+print(created_date)
+print(issue_date)
