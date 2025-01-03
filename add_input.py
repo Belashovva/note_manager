ff34de356990032ba156ca62621ff842e9bf6375
Belashovva <belashova.com@mail.ru>
2025-01-03 17:08:17 +0300

    Add files via upload

---
+++ add_input.py
@@ -0,0 +1,13 @@
+username=input('Введите имя пользователя ')
+title=input('Введите название заметки ')
+content=input('Введите текст ')
+status=input("Введите статус задачи ")
+created_date=input("Дата создания'дд.мм.гггг'")
+issue_date=input("Дата истечения заметки'дд.мм.гггг'")
+
+print(username)
+print(title)
+print(content)
+print(status)
+print(created_date)
+print(issue_date)
