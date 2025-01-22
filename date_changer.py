1bb739d0297a61c481d2157f3fc021d2bddc2bcc
Belashovva <belashova.com@mail.ru>
2025-01-22 09:45:29 +0300

    date_changer.py

--- date_changer.py
+++ date_changer.py
@@ -1,3 +1,9 @@
-created_date = '03.01.2025'
+created_date=input("Дата создания'дд.мм.гггг' ")
+issue_date=input("Дата истечения заметки'дд.мм.гггг' ")
+
+
 temp_created_date = created_date[:5]
-print(temp_created_date)
+print("Дата создания заметки:", temp_created_date)
+temp_issue_date = issue_date[:5]
+print("Дата истечения заметки:", temp_issue_date)
+
