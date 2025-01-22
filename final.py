5fb175a4628dc195977992abd180fb2b9b2d587a
Belashovva <belashova.com@mail.ru>
2025-01-22 10:04:39 +0300

    Update final.py

--- final.py
+++ final.py
@@ -1,29 +1,15 @@
-username=input('Введите имя пользователя ')
-title0=input('Введите название заметки ')
-title1=input('Заголовок 1 ')
-title2=input('Заголовок 2 ')
-title3=input('Заголовок 3 ')
-content=input('Введите текст ')
-status=input("Введите статус задачи ")
-created_date=input("Дата создания'дд.мм.гггг' ")
-issue_date=input("Дата истечения заметки'дд.мм.гггг' ")
+note = {}
 
-print(username)
-print(title0)
-print(title1)
-print(title2)
-print(title3)
-print(content)
-print(status)
-print(created_date)
-print(issue_date)
+note["username"] = input('Введите имя пользователя ')
+note["content"] = input('Введите текст ')
+note["status"] = input("Введите статус задачи ")
+note["created_date"] = input("Дата создания'дд.мм.гггг' ")
+note["issue_date"] = input("Дата истечения заметки'дд.мм.гггг' ")
 
-note=[
-username,
-content,
-status,
-created_date,
-issue_date,
-[title0, title1, title2, title3]]
+titles = []
+for i in range(3):
+    title = input(f"Введите заголовок заметки {i + 1}: ")
+    titles.append(title)
 
 print(note)
+
